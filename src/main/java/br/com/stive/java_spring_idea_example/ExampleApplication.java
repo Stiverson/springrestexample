dd92299ca466f13c5ad2db53bb430468be39e540
smachado <stiverson.machado@gmail.com>
2021-10-22 10:43:57 -0300

    novas classe e funcionalidade

--- src/main/java/br/com/stive/java_spring_idea_example/ExampleApplication.java
+++ src/main/java/br/com/stive/java_spring_idea_example/ExampleApplication.java
@@ -1,5 +1,7 @@
 package br.com.stive.java_spring_idea_example;
 
+import br.com.stive.java_spring_idea_example.pojo.Cidade;
+import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,41 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class ExampleApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ExampleApplication.class, args);
+
+        Cidade piracicaba = new Cidade();
+        piracicaba.setNome("Piracicaba Cidade da Pamonha");
+        piracicaba.setNumeroHabitantes(5655841);
+        piracicaba.setNumeroEdificios(868);
+        piracicaba.setNomePrefeito("Luciano Almeida");
+        piracicaba.setCidadeMetropolitana(false);
+
+        Gson gson = new Gson();
+        String jsonPiracicaba = gson.toJson(piracicaba);
+        System.out.println(jsonPiracicaba);
+
+        Cidade jaguariuna = new Cidade();
+        jaguariuna.setNome("Cidade Jaguariuna");
+        jaguariuna.setNumeroHabitantes(80000);
+        jaguariuna.setNumeroEdificios(200);
+        jaguariuna.setNomePrefeito("Mikael Alimofótons");
+        jaguariuna.setCidadeMetropolitana(true);
+
+        String jsonJaguariuna = gson.toJson(jaguariuna);
+        System.out.println(jsonJaguariuna);
+
+        Cidade campinas = new Cidade();
+        campinas.setNome("Cidade Campinas");
+        campinas.setNumeroHabitantes(1200000);
+        campinas.setNumeroEdificios(200000);
+        campinas.setNomePrefeito("Dário Saad");
+        campinas.setCidadeMetropolitana(true);
+
+        String jsonCampinas = gson.toJson(campinas);
+        System.out.println(jsonCampinas);
+
+
+
+
+        //SpringApplication.run(ExampleApplication.class, args);
     }
 }
---
+++ src/main/java/br/com/stive/java_spring_idea_example/pojo/Cidade.java
@@ -0,0 +1,50 @@
+package br.com.stive.java_spring_idea_example.pojo;
+
+public class Cidade {
+
+    private int numeroHabitantes;
+    private int numeroEdificios;
+    private String nome;
+    private String nomePrefeito;
+    private boolean cidadeMetropolitana;
+
+    public boolean isCidadeMetropolitana() {
+        return cidadeMetropolitana;
+    }
+
+    public void setCidadeMetropolitana(boolean cidadeMetropolitana) {
+        this.cidadeMetropolitana = cidadeMetropolitana;
+    }
+
+    public int getNumeroHabitantes() {
+        return numeroHabitantes;
+    }
+
+    public void setNumeroHabitantes(int numeroHabitantes) {
+        this.numeroHabitantes = numeroHabitantes;
+    }
+
+    public int getNumeroEdificios() {
+        return numeroEdificios;
+    }
+
+    public void setNumeroEdificios(int numeroEdificios) {
+        this.numeroEdificios = numeroEdificios;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomePrefeito() {
+        return nomePrefeito;
+    }
+
+    public void setNomePrefeito(String nomePrefeito) {
+        this.nomePrefeito = nomePrefeito;
+    }
+}
