7e8821beb4630d245e52441d57ea29cb4beb325b
Gabriela <gabrielamt@ciandt.com>
2021-10-22 14:47:09 -0300

    aprendendo commit via intelliJ

---
+++ src/main/java/br/com/stive/java_spring_idea_example/pojo/Paises.java
@@ -0,0 +1,28 @@
+package br.com.stive.java_spring_idea_example.pojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Paises{
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Estado> getListaEstados() {
+        return listaEstados;
+    }
+
+    public void setListaEstados(List<Estado> listaEstados) {
+        this.listaEstados = listaEstados;
+    }
+
+    private String nome;
+
+    private List<Estado> listaEstados = new ArrayList<>();
+
+
+}
